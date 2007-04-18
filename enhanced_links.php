569d1e0a3c00697989fd014c813381c18bfcffed
vprat <vprat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-04-18 20:44:45 +0000

    First commit
    git-svn-id: https://plugins.svn.wordpress.org/enhanced-links/trunk@11336 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ enhanced_links.php
@@ -0,0 +1,175 @@
+<?php
+/*
+Plugin Name: enhanced links
+Version: 2.0.1
+Plugin URI: http://www.vincentprat.info/wordpress/category/coding
+Description: Allows to get better control over the links listing : edit the links.template.inc file if you're not satisfied with the formatting. 
+Author: Vincent Prat
+Author URI: http://www.vincentprat.info
+*/
+
+/*  Copyright 2006 Vincent Prat  (email : vpratfr@yahoo.fr)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/********** YOU CAN EDIT THE FOLLOWING VALUES *************/
+
+// The symbol to display when the category can be shown, should be a string
+define('SHOW_SYMBOL', '&raquo;'); 
+
+// The symbol to display when the category can be hidden, should be a string
+define('HIDE_SYMBOL', '&laquo;'); 
+
+// Set this to true if you want to show the symbol before the category name. Else set to false.
+define('IS_SYMBOL_BEFORE', true); 
+
+// Set this to true if you want to show the link description.
+define('SHOW_LINK_DESCRIPTION', false); 
+
+// Set this to true if you want to use scriptaculous effects. You need to install the wp-scriptaculous
+// plugin. See there: http://www.silpstream.com/blog/
+define('USE_SCRIPTACULOUS_EFFECTS', false); 
+
+
+
+
+/********** YOU SHOULD NOT NEED TO EDIT WHAT IS BELOW THIS LINE *************/
+/********** IF YOU DO, BE CAREFUL ;) *************/
+
+/**
+ * show the links
+ */
+function enh_links_show_enhanced_links() {	
+	// Get the category
+	$categories = enh_links_get_categories();
+	
+	// Insert the javascript in the page
+	enh_links_insert_javascript($categories);
+	
+	// Insert the categories
+	enh_links_insert_html_categories($categories);
+}
+
+/**
+ * Get the list of categories 
+ */ 
+function enh_links_get_categories() {
+	global $wpdb;
+	
+	if (isset($wpdb->linkcategories)) {
+		// For wordpress 1.5.x and 2.0.x
+		return $wpdb->get_results(
+			"SELECT cat_id, cat_name 
+			 FROM $wpdb->linkcategories");
+	} else {
+		// For wordpress v2.1+
+		return $wpdb->get_results(
+			"SELECT cat_id, cat_name 
+			 FROM $wpdb->categories WHERE link_count>0");
+	}
+}
+
+/**
+ * Function to insert the html code for the categories and links
+ */
+function enh_links_insert_html_categories($categories) {
+?>		
+<?php
+	foreach ($categories as $cat) {	
+?>
+		<h2><?php 
+			if (IS_SYMBOL_BEFORE) {
+				echo '<a id="categoryButton'. $cat->cat_id . '">' . SHOW_SYMBOL . '</a> &nbsp; ';
+			}
+		?><a href='javascript:expandCategory(<?php echo $cat->cat_id; ?>);' title='<?php echo __("Show/Hide links"); ?>'><?php echo $cat->cat_name ?></a><?php 
+			if (!IS_SYMBOL_BEFORE) {
+				echo '&nbsp;<a id="categoryButton'. $cat->cat_id . '">' . SHOW_SYMBOL . '</a>';
+			}
+		?>
+		</h2>
+		<div id="categoryContent<?php echo $cat->cat_id; ?>" style="DISPLAY:none">
+			<ul>
+				<?php get_links( 
+					$cat->cat_id, 
+					'<li>',       
+					'</li>',      
+					'<br/>',  
+					false, 'name', SHOW_LINK_DESCRIPTION, false, -1, 1, true ); ?>
+			</ul>
+		</div>  	
+<?php
+	} 		
+?>		
+<?php
+}
+
+/**
+ * Function to insert the javascript 
+ */
+function enh_links_insert_javascript($categories) {
+?>
+<script language="javascript">
+	var js_categories = new Array(<?php echo count($categories); ?>);
+<?php
+	// Declare the JS variables
+	$i = 0;
+	foreach ($categories as $cat) {	
+		echo "\tjs_categories[$i] = new Array($cat->cat_id, '$cat->cat_name');\n";
+		$i++;
+	}
+?>
+
+	function expandCategory(categoryId) {
+		symbolBeforeCatName = <?php if (IS_SYMBOL_BEFORE) echo "true"; else echo "false"; ?>;
+		showSymbol = <?php echo "'".SHOW_SYMBOL."'"; ?>;
+		hideSymbol = <?php echo "'".HIDE_SYMBOL."'"; ?>;
+		useScriptaculousEffects = <?php if (USE_SCRIPTACULOUS_EFFECTS) echo "true"; else echo "false"; ?>;
+		
+		for (i=0; i<js_categories.length; i++) {
+			currentId = js_categories[i][0];
+			currentName = js_categories[i][1];
+			
+			currentButton = document.getElementById('categoryButton' + currentId);
+			currentContent = document.getElementById('categoryContent' + currentId);
+			
+			if (currentId==categoryId) {
+				// Expand this category								
+				if (currentButton.innerHTML != hideSymbol) {
+					if (useScriptaculousEffects) {
+						new Effect.BlindDown(currentContent);
+					} else {
+						currentContent.style.display = 'block';
+					}
+				}
+				
+				currentButton.innerHTML = hideSymbol;
+			} else {
+				// Contract this category								
+				if (currentButton.innerHTML != showSymbol) {
+					if (useScriptaculousEffects) {
+						new Effect.BlindUp(currentContent);
+					} else {
+						currentContent.style.display = 'none';
+					}
+				}
+				currentButton.innerHTML = showSymbol;
+			}
+		}
+	}
+</script>
+<?php
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Enhanced Links ===
+Contributors: vprat
+Donate link: http://www.vincentprat.info/wordpress/2006/04/13/wordpress-plugin-enhanced-links/
+Tags: mail, users, list
+Requires at least: 1.5.0
+Tested up to: 2.1
+Stable tag: 2.0.1
+
+A plugin for wordpress which allows you to list your links in a sexier way. Very useful when you have a great number of links and categories.
+
+== Description ==
+
+A plugin for wordpress which allows you to list your links in a sexier way. Very useful when you have a great number of links and categories. A sample use can be found on my [links page] (http://www.vincentprat.info/wordpress/wp-content/themes/vprat2/links.php).
+
+This plugin is available under the GPL license, which means that it’s free. If you use it for a commercial web site, if you appreciate my efforts or if you want to encourage me to develop and maintain it, please consider making a donation using Paypal, a secured payment solution. You just need to click the button on the [plugin home page] (http://www.vincentprat.info/wordpress/2006/04/13/wordpress-plugin-enhanced-links/) and follow the instructions.
+
+== Installation ==
+
+Just install the plugin in the wordpress “wp-content/plugins/enhanced_links/” directory, activate it and insert some little code into the side bar:
+
+`<li><h2>Links</h2>`
+`<?php enh_links_show_enhanced_links(); ?>`
+`</li>`
+
+**Options**
+
+By editing the plugin file, you will be able to customize a little bit the listing of the links, for example if you want to show an image instead of the », or if you want to show the link description or if you want to use the possibilities offered by scriptaculous effects allows to smoothly slide in and out the link blocks.
